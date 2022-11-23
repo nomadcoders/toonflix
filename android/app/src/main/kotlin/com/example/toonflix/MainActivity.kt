bd7cdfbc8cd8d929f767ee352ccacd3dc3fa4eb1
니꼬 <nico@nomadcoders.co>
2022-11-23 16:24:43 +0900

    1.3 Hello World

---
+++ android/app/src/main/kotlin/com/example/toonflix/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.toonflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello flutter!'),
+        ),
+        body: Center(
+          child: Text('Hello world!'),
+        ),
+      ),
+    );
+  }
+}
